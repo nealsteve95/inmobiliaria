0955ec08b00a56730720ba77e63abe87b5cd87a6
Neals Paye <neals.paye@tecsup.edu.pe>
2023-05-04 00:17:36 -0500

    Cambio color encabezado

--- adicional/encabezado.php
+++ adicional/encabezado.php
@@ -5,16 +5,19 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style/estilo.css">
 
     <!-- Bootstrap CSS v5.0.2 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <!-- cdn icnonos-->
+    <!-- Son los icnonos para borrar y editar-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   </head>
   <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-      <div class="container-fluid bg-warning">
+    <!--Interacción JS-->
+    <!--bg-warning es el color-->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+      <div class="container-fluid bg-primary bg-gradient text-white bg-opacity-85">
           <div class="row">
               <div class="col-md">
                   <header class="py-3">
